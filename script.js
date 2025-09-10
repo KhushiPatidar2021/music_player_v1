6f2fc52b06ce07dd7f7029e5df90e480d26a7617
Khushi Patidar <khushipatidar.013@gmail.com>
2025-09-10 10:36:06 +0530

    finished

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const form = document.querySelector("form");
 let inputs = document.querySelectorAll("input");
 let main = document.querySelector("main");
-let hero =document.querySelector(".hero")
-let org_form =document.querySelector(".org-form");
+let hero = document.querySelector(".hero")
+let org_form = document.querySelector(".org-form");
 let icon2 = document.querySelector(".icon2");
-let form_container= document.querySelector(".form-container");
+let form_container = document.querySelector(".form-container");
 let icon = document.querySelector(".icon");
 let favcounter = document.querySelector(".favcount");
-let favcount =0;
+let favcount = 0;
 
-icon.addEventListener("click", function(){
-    form_container.style.display="block";
-    hero.style.display="none"
+icon.addEventListener("click", function () {
+    form_container.style.display = "block";
 })
 
 
@@ -25,51 +24,50 @@ form.addEventListener("submit", function (dets) {
     profile.classList.add("profile");
 
     const h2 = document.createElement("h2");
-  h2.textContent = `Artist : ${inputs[0].value}`;
+    h2.textContent = `Artist : ${inputs[0].value}`;
 
     const h3 = document.createElement("h3");
- h3.textContent = `Song Name : ${inputs[1].value}`;
+    h3.textContent = `Song Name : ${inputs[1].value}`;
 
     const img = document.createElement("img");
-    img.setAttribute("src", inputs[2].value );
+    img.setAttribute("src", inputs[2].value);
 
     const rmvbtn = document.createElement("button");
-rmvbtn.classList.add("remove");
-rmvbtn.textContent="x"
-
-    
-
-     rmvbtn.addEventListener("click", function () {
-      card.remove();
-      favcount--;
-      favcounter.textContent=favcount;
-})
-
-profile.appendChild(img);
-
-card.appendChild(rmvbtn);
-card.appendChild(profile);
-
-card.appendChild(h2);
-card.appendChild(h3);
-
-hero.appendChild(card);
-
-favcount++;
-favcounter.textContent= favcount;
-
-inputs.forEach(function(inp){
-    inp.value="";
-});
-
-});
+    rmvbtn.classList.add("remove");
+    rmvbtn.textContent = "x"
 
 
-icon2.addEventListener("click", function(){
-    form_container.style.display="none";
-    hero.style.display="flex";
+
+    rmvbtn.addEventListener("click", function () {
+        card.remove();
+        favcount--;
+        favcounter.textContent = favcount;
+    })
+
+    profile.appendChild(img);
+
+    card.appendChild(rmvbtn);
+    card.appendChild(profile);
+
+    card.appendChild(h2);
+    card.appendChild(h3);
+
+    hero.appendChild(card);
+
+    favcount++;
+    favcounter.textContent = favcount;
+
+    inputs.forEach(function (inp) {
+        inp.value = "";
+    });
+
 });
 
 
+icon2.addEventListener("click", function () {
+    form_container.style.display = "none";
+    hero.style.display = "flex";
+});
 
-    
+
+
